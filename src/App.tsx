289cb04cd92b9d6a8188653cbb00b726c3bdbe3f
LuizHenrique0001 <rodriguesluizhenrique316@gmail.com>
2025-03-10 11:19:22 -0300

    Criacao do module Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
-import './App.css'
-import styled from 'styled-components'
-
-const DivTeste = ({name})=> {
-  return(
-    <div>
-      <h1>Seja bem vindo {name}</h1>
-    </div>
-  )
-};
-
+import LoginScreen from './modules/login';
+    
 function App() {
 
   return (
-    <div>
-      
-      <DivTeste name="luiz" />
-      <DivTeste name="maria" />
-      <DivTeste name="joao" />
-      <h1>Ola Mundo!</h1>
-    </div>
+   <LoginScreen />
   )
 }
-
-interface PStyleLink {
-  isBlue?: boolean
-}
-
-const StyledLink = styled.div<PStyleLink>`
-color:${(props) => (props.isBlue ? 'red':'blue')};
-font-weight: bold;
-`;
 
 export default App
---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import { BeckgrountImage } from "./styles/stylesScreen.stayles"
+
+const LoginScreen = () => {
+
+    return (
+        <div>
+            <BeckgrountImage src="./background.png" />
+        </div>
+    )
+}
+
+export default LoginScreen
